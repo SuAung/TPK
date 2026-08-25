99c2dd038b0c1514838700ef5ca588fb45f4fc9a
SuAung <susuaung87@gmail.com>
2026-08-25 12:12:47 +0900

    Update port

--- backend/server.js
+++ backend/server.js
@@ -58,4 +58,7 @@ ${inquiry}
 });
 
 
-app.listen(3000, () => console.log("Backend running on port 3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Backend running on port ${process.env.PORT || 3000}`)
+);
+
